e41a2d559912f989ff01e66958ec4987de00857e
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-01-20 11:31:15 +0530

    Update Glass.js

--- Glass.js
+++ Glass.js
@@ -1,27 +1,9 @@
-class Glass {
-  constructor(x, y, width, height) {
-    var options = {
-        'restitution':0.8,
-        'friction':1.0,
-        'density':1.0
-    }
-    this.body = Bodies.rectangle(x, y, width, height, options);
-    this.width = width;
-    this.height = height;
+ 
+
+class Glass extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
     this.image = loadImage("sprites/glass.png");
-
-    World.add(world, this.body);
   }
-  display(){
-    var pos =this.body.position;
-    var angle = this.body.angle;
-    push();
-    translate(pos.x, pos.y);
-    rotate(angle);
-    imageMode(CENTER)
-    //rect( 0, 0, this.width, this.height);
 
-    image(this.image,0, 0, this.width, this.height);
-    pop();
-  }
 };
